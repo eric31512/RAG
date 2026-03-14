0861cdb87bba07f9b5b59bb97b8f90b2122254c9
Chi-Chieh Tsai <eric31512@gmail.com>
2026-03-14 14:41:01 +0800

    contextual kg all

--- chunk.sh
+++ chunk.sh
@@ -3,5 +3,4 @@ source .venv/bin/activate
 python src/build_kg_from_chunks.py \
     --lang 'en' \
     --chunk_cache "cache/chunk_cache/en_contextual_chunksize512" \
-    --output "graph/prefix_original_kg_all" \
-    --use_prefix
+    --output "graph/original_contextual_kg_all" \
